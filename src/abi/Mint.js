c0517c8cd7d9ab9b9e9e4a4b0854036df5aa7a6e
Antonio Sze-To <antonioszeto@gmail.com>
2023-06-04 14:35:07 -0400

    implemented mint button

---
+++ src/abi/Mint.js
@@ -0,0 +1,15 @@
+const abi = [
+  {
+    inputs: [{ internalType: "uint256", name: "quantity", type: "uint256" }],
+    name: "mint",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+];
+
+const contractAddress = {
+  sepolia: "0x7f47b9d45cc272b3b624b552fe4f198cd2e3a874",
+};
+
+export { abi, contractAddress };
--- src/components/MintButton.js
+++ src/components/MintButton.js
@@ -1,4 +1,4 @@
-import { Button, Center, Text } from "@chakra-ui/react";
+import { Button, Center, Text, Link } from "@chakra-ui/react";
 import {
   NumberInput,
   NumberInputField,
@@ -6,16 +6,70 @@ import {
   NumberIncrementStepper,
   NumberDecrementStepper,
 } from "@chakra-ui/react";
+import { useState } from "react";
+import {
+  useContractWrite,
+  usePrepareContractWrite,
+  useWaitForTransaction,
+  useNetwork,
+} from "wagmi";
+import { parseEther } from "viem";
+import { abi, contractAddress } from "../abi/Mint";
 
 const MintButton = () => {
+  const [mintValue, setMintValue] = useState(1);
+  const { chain } = useNetwork();
+
+  const { config } = usePrepareContractWrite({
+    address: contractAddress[chain.network],
+    abi: abi,
+    functionName: "mint",
+    args: [mintValue],
+    value: parseEther(String(0.001 * mintValue)),
+  });
+
+  const contractWrite = useContractWrite(config);
+
+  const waitForTransaction = useWaitForTransaction({
+    hash: contractWrite.data?.hash,
+  });
+
+  const handleSendTransaction = async () => {
+    try {
+      await contractWrite.writeAsync();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <Text mb={10}>
-        Hey! You can mint your NFT here! You can either mint 1-3 NFTs. Currently
-        only Sepolia network is supported.
+        Hey! You can either mint 1 - 3 NFT here. Each NFT costs 0.001 ETH.
       </Text>
+      {waitForTransaction.isLoading && <Text mb={10}>Minting NFT...</Text>}
+      {waitForTransaction.isSuccess && (
+        <Text mb={10}>
+          <Link
+            href={`${chain?.blockExplorers?.default.url}/tx/${contractWrite.data?.hash}`}
+            isExternal
+          >
+            Your mint is successful! Click me to check your transaction
+          </Link>
+        </Text>
+      )}
+      {(contractWrite.isError || waitForTransaction.isError) && (
+        <Text mb={10}>Something wrong during minting...please try again</Text>
+      )}
 
-      <NumberInput defaultValue={1} min={1} max={3} mb={5}>
+      <NumberInput
+        defaultValue={1}
+        min={1}
+        max={3}
+        mb={5}
+        value={mintValue}
+        onChange={(mintValueString) => setMintValue(mintValueString)}
+      >
         <NumberInputField />
         <NumberInputStepper>
           <NumberIncrementStepper />
@@ -24,10 +78,17 @@ const MintButton = () => {
       </NumberInput>
 
       <Center>
-        <Button colorScheme="teal" size="lg">
+        <Button
+          isDisabled={waitForTransaction.isLoading || !contractWrite.writeAsync}
+          colorScheme="teal"
+          size="lg"
+          onClick={handleSendTransaction}
+        >
           Mint
         </Button>
       </Center>
+
+      <Text fontSize="lg" fontWeight="bold"></Text>
     </>
   );
 };
